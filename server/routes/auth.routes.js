d6406c3fe7cb531bb161cd26bec3a1659a5497c6
Anish S Shetty <anishshetty124@gmail.com>
2025-12-07 14:28:01 +0530

    Update Google auth redirect URLs

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,7 +1,5 @@
 import { Router } from 'express';
 import passport from 'passport';
-import { User } from '../models/user.model.js';
-import { ApiResponse } from '../utils/ApiResponse.js';
 
 const router = Router();
 
@@ -10,15 +8,13 @@ router.get('/google', passport.authenticate('google', { scope: ['profile', 'emai
 router.get(
   '/google/callback',
   passport.authenticate('google', {
-    failureRedirect: `${process.env.CORS_ORIGIN}/login`, 
+    failureRedirect: `${process.env.FRONTEND_URL}/login`,
     session: false,
   }),
   (req, res) => {
     const user = req.user;
     const accessToken = user.generateAccessToken();
-    
-   
-    res.redirect(`${process.env.CORS_ORIGIN}/auth/success?token=${accessToken}`);
+    res.redirect(`${process.env.FRONTEND_URL}/auth/success?token=${accessToken}`);
   }
 );
 
